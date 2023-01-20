110379735c06d77faeb1b559b1ab793a6b074801
Edmundo Pichardo <ce.pichardo@gmail.com>
2023-01-20 05:03:34 -0400

    Se configuro primer menu de opciones

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 
-import { leerInput } from "./helpers/inquirer.js"
+import { inquirerMenu, leerInput, pausa } from "./helpers/inquirer.js"
 
 const main = async () => {
-   const text = await leerInput('Hola:'.green);
-   console.log(text);
+   // const text = await leerInput('Hola:'.green);
+   // console.log(text);
+   let opt = '';
+   do {
+      // console.clear();
+      opt = await inquirerMenu();
+      console.log({opt} );
+      console.log('\n');
+      if( opt !==0 ) await pausa();
+   }while(opt !== 0) 
 }
 
 main();
