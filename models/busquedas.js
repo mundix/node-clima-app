0fe468acd241a6a28f0d3fd3ff40b561ed62e5b4
Edmundo Pichardo <ce.pichardo@gmail.com>
2023-01-21 07:51:11 -0400

    Se refactorizo los params de mapbox

--- models/busquedas.js
+++ models/busquedas.js
@@ -4,28 +4,42 @@ import axios from "axios";
 class Busquedas {
    historial = ['Tegucigalpa', 'Madrid', 'Santo Domingo', 'San Jose'];
 
-   constructor () {
+   constructor() {
       //Leer DB Si existe
 
+   }
+
+   get paramsMapBox() {
+      return {
+         access_token: 'pk.eyJ1IjoibXVuZGl4MDEiLCJhIjoiY2xkNGJ0ZGQ1MDB0ajN2cGNwYmszZ3B3eSJ9.ORtvYlHRQmYXOlPZH8hmRg',
+         limit: 5,
+         language: 'es'
+      }
    }
 
    // Sera async por que voy a usar unafunciona http
    //REgresa un arreglo con todas las ciudades o lugares que coincida 
    //con este lugar 
-   async ciudad( lugar ='') {
-
-      //Peticion HTTP
-      // console.log(lugar);
+   async ciudad(lugar = '') {
       try {
-         const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Madrid.json?access_token=pk.eyJ1IjoibXVuZGl4MDEiLCJhIjoiY2xkNGJ0ZGQ1MDB0ajN2cGNwYmszZ3B3eSJ9.ORtvYlHRQmYXOlPZH8hmRg&limit=5&language=es');
-         console.log(resp.data);   
+
+         //Peticion HTTP
+         const instance = axios.create({
+            baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+            params: this.paramsMapBox
+         });
+
+         const resp = await instance.get();
+         console.log(resp.data);
+
          return [];
+
       } catch (error) {
          return [];
       }
-      
 
-      
+
+
    }
 
 }
